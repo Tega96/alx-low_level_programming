539cec78e7ea7bb730aa8daa7bfa0e53df6dac65
Tega96 <otiteotega@gmail.com>
2024-02-19 09:56:50 -0800

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This is a description
+ * Return = Always 0
+ */
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
