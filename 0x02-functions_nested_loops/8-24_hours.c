f182bcd07dbd6f5ff4d7bbf30864f4e116c853da
Tega96 <otiteotega@gmail.com>
2024-02-24 10:01:52 -0800

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,41 +1,42 @@
 #include "main.h"
 
 /**
- * jack_bauer - Prints every minutes of the day of jack bauer
+ * jack_bauer - Prints every minute of the day of Jack Bauer,
+ *              starting from 00:00 to 23:59.
  */
 
 void jack_bauer(void)
 {
-int min, hr;
+	int min, hr;
 
-hr = -1;
+	hr = -1;
 	while (hr++ < 23)
 	{
 		min = -1;
-			while (min++ < 59)
+		while (min++ < 59)
+		{
+			if (hr <= 9)
 			{
-				if (hr <= 9)
-				{
-					_putchar('0');
-					_putchar(hr + '0');
-				}
-				else
-				{
-					_putchar(hr / 10 + '0');
-					_putchar(hr % 10 + '0');
-				}
-				_putchar(':');
-				if (min <= 9)
-				{
-					_putchar('0');
-					_putchar(min + '0');
-				}
-				else
-				{
-					_putchar(min / 10 + '0');
-					_putchar(min % 10 + '0');
-				}
-				_putchar('\n');
-
+				_putchar('0');
+				_putchar(hr + '0');
+			}
+			else
+			{
+				_putchar(hr / 10 + '0');
+				_putchar(hr % 10 + '0');
+			}
+			_putchar(':');
+			if (min <= 9)
+			{
+				_putchar('0');
+				_putchar(min + '0');
+			}
+			else
+			{
+				_putchar(min / 10 + '0');
+				_putchar(min % 10 + '0');
+			}
+			_putchar('\n');
+		}
+	}
 }
-
