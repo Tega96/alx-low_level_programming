46cba8524f56cb294942d898cc7264600ca47f3f
Tega96 <otiteotega@gmail.com>
2024-02-24 10:10:23 -0800

    add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns the result.
+ * @f_int: First integer
+ * @s_int: Second integer
+ *
+ * Return: sum of @f_int + @s_int.
+ */
+
+int add(int f_int, int s_int)
+{
+	return (f_int + s_int);
+}
