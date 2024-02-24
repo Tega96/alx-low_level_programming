9209ee42245602e03dc8c67564a20f7233993752
Tega96 <otiteotega@gmail.com>
2024-02-24 07:06:12 -0800

    putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - Prints all the lowercase alphabets
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
