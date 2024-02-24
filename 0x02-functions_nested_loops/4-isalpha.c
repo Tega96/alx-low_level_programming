6f64e9075ce84c7bd2c41ccf31492df5e58f4d5d
Tega96 <otiteotega@gmail.com>
2024-02-24 10:52:45 -0800

    print alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main"
+#include "main.h"
 
 /**
  * _isalpha - This code checks alphabetic characters
@@ -9,7 +9,7 @@
 
 int _isalpha(int c)
 {
-if ((c >64 && <91) || (c > 96 && < 123))
+if ((c > 64 && c < 91) || (c > 96 && c < 123))
 	return (1);
 else
 	return (0);
