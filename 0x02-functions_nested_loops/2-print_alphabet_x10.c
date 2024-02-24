1dacbf14489548ac2d434c7fe292dbd53f96f943
Tega96 <otiteotega@gmail.com>
2024-02-24 07:47:16 -0800

    print numbers

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,8 +7,20 @@
  * Return: Always 0
  */
 
-int main(void)
+void print_alphabet_x10(void)
 {
-print_alphabet_x10();
-return (0);	
+char c;
+int i;
+
+for (i = 0; i < 10; i++)
+{
+	c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
 }
+}
+
