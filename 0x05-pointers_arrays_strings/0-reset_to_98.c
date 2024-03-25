9ffb12c05f1ed672451fb384b79c84d947072f51
Tega96 <otiteotega@gmail.com>
2024-03-25 01:04:06 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value of a pointer
+ * @n: a pointer to an int
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is to practice the concept of pointers, arrays and strings
