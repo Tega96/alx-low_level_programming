82d9877cb62576717c44c459f8e0b16209f7a1ee
Tega96 <otiteotega@gmail.com>
2024-06-12 17:44:37 -0700

    printing _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,6 +7,15 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	/*printf("_putchar\n");*/
 	return (0);
 }
