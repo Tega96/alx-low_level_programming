e99a23d7529b3fc9c1c04dce717a1e8f86814860
Tega96 <otiteotega@gmail.com>
2024-06-15 18:08:35 -0700

    prints a string to stdout

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,3 +1,17 @@
 #include "main.h"
 
+/**
+ * puts - prints a string
+ * @str: the parameter used
+ */
 
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}	
+	_putchar('\n');
+		
+}
