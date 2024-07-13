8627207c94fcfeaf32e603a5a018a74ed3326d0c
Tega96 <otiteotega@gmail.com>
2024-07-13 05:02:58 -0700

    print chess board

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - this function prints a chess board
+ * @a: the pointer to the piece to print
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
